32aa15ff36588cbed5cb25bb01fe04a41deb405a
Olivier Froment <olivier_froment@hotmail.com>
2021-03-10 01:10:45 -0500

    nav bar anim

--- styles/typewriter.js
+++ styles/typewriter.js
@@ -1,23 +1,29 @@
-const texts = ['Olivier Froment'];
+const texts = ["Bonjour!", "Je m\'appelle Olivier"];
 let count = 0;
 let index = 0;
 let currentText = '';
 let letter = '';
 
+
 (function type(){
 
     currentText = texts[count];
-
-
+    console.log(currentText);
     letter = currentText.slice(0, ++index);
+ 
 
-
+    if (count ==0){
     document.querySelector('.typing').textContent = letter;
+} else if (count == 1) {
+    document.querySelector('.typingTwo').textContent = letter;
+}
+
     if (letter.length === currentText.length){
         count++;
-        index = 0 ;
-
+        index = 0;
+        setTimeout(10000);
     }
-    setTimeout(type, 75);
+
+    setTimeout(type, 100);
 
 }());
